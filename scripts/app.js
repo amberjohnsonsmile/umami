28a09051a7f0f89230bef3a157b23540a370dc72
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2017-11-18 22:30:02 -0700

    Refine site styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,11 @@
 window.addEventListener("scroll", function() {
-	document.querySelector(".logo .first-half").remove();
-	document.querySelector(".logo .second-half").textContent = "umami's";
+	if (document.querySelector(".logo .second-half").textContent != "umami's") {
+		document.querySelector(".logo .first-half").remove();
+		document.querySelector(".logo .second-half").textContent = "umami's";
+		document.querySelector(".logo .second-half").style.fontWeight = 600;
+	}
+});
+
+document.querySelector("button").addEventListener("click", function() {
+	document.querySelector("#stream").style.display = "inherit";
 });
